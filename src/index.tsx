99555c593ae78b04e86f37b422f12b4231de853b
elfexor <imbalance777@gmail.com>
2020-10-26 21:32:41 +0300

    webpack loaders

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class Nyma extends Component {
+	render() {
+		return (
+			<div className="" />
+		);
+	}
+}
+
+
+ReactDOM.render(<Nyma/>, document.getElementById('root'));
