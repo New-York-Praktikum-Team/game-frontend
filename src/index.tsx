c7f8eb0adca45bb4300397ae670b59eb92fe4e3b
elfexor <imbalance777@gmail.com>
2020-10-31 10:18:26 +0300

    husky added

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,4 @@ import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './components/App';
 
-
-
 ReactDOM.render(<StrictMode><App /></StrictMode>, document.getElementById('root'));
-
-
-
-
