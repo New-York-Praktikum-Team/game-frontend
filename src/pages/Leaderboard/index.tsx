77a6f759daf1d7b4f63d665d3d868d9e490dd510
Irina Tishchenko <irin.tishchenko@gmail.com>
2020-11-07 23:28:02 -0600

    couople more codereview fixes

--- src/pages/Leaderboard/index.tsx
+++ src/pages/Leaderboard/index.tsx
@@ -22,13 +22,13 @@ export const Leaderboard: FC = () => {
     <section className='leaderboard-wrapper'>
       <h1>Leaderboard</h1>
 
-      {leaderData.length > 0
+      {leaderData.length
         ? <div className='leader-table'>
           {leaderData.map((leader, index) => (
             <div className='row' key={index}>
               <div className='col'>{index + 1}</div>
               <div className='col'>{leader.displayName}</div>
-              <div className='col'><img className='image' src={dollar} />&nbsp;{leader.points}</div>
+              <div className='col'><img className='image' src={dollar} alt='dollar' />&nbsp;{leader.points}</div>
             </div>
           ))}
         </div>
