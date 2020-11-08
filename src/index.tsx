c6d9d31f591026ca274a7b255f1f06c4edc80cc4
Irina Tishchenko <irin.tishchenko@gmail.com>
2020-11-08 17:51:04 -0600

    changing relative paths to absolute using tsconfig-paths-webpack-plugin

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './components/App';
+import { App } from 'components/App';
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize.min';
 
