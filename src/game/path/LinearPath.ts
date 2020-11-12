4097c75568428a8cbc5940e6120f5e3cae70f6b0
Irina Tishchenko <irin.tishchenko@gmail.com>
2020-11-12 14:24:15 -0600

    fixing more linearPath bugs, moving direction from start to end

--- src/game/path/LinearPath.ts
+++ src/game/path/LinearPath.ts
@@ -3,16 +3,21 @@ import { Position } from '../Position';
 
 export class LinearPath extends Path {
   public next(current: Position, distanceDelta: number): Position {
-    let k;
-
-    if (this.end.y === this.start.y) {
-      k = 0;
-    } else if (this.end.x === this.start.x) {
-      throw new Error("don't do that");
-    } else {
-      k = (this.end.y - this.start.y) / (this.end.x - this.start.x);
+    if ((this.start.x === this.end.x && this.start.y === this.end.y) || distanceDelta === 0) {
+      throw new Error('Not enough data to calculate path');
     }
 
+    if (this.end.x === this.start.x) {
+      let newY;
+      if (this.start.y < this.end.y) {
+        newY = current.y + distanceDelta;
+      } else {
+        newY = current.y - distanceDelta;
+      }
+      return { x: current.x, y: newY };
+    }
+
+    const k = (this.end.y - this.start.y) / (this.end.x - this.start.x);
     const b = this.start.y - k * this.start.x;
 
     if (current.y.toFixed(2) !== (k * current.x + b).toFixed(2)) {
@@ -20,7 +25,12 @@ export class LinearPath extends Path {
       console.log("Current position isn't on the path");
     }
 
-    const newX = current.x + Math.cos(Math.asin(k)) * distanceDelta;
+    let newX;
+    if (this.start.x < this.end.x) {
+      newX = current.x + Math.cos(Math.atan(k)) * distanceDelta;
+    } else {
+      newX = current.x - Math.cos(Math.atan(k)) * distanceDelta;
+    }
     const newY = k * newX + b;
 
     return { x: newX, y: newY };
