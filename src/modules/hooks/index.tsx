85c2906ebf66a09435b24840179aae7c7b42372c
elfexor <imbalance777@gmail.com>
2020-11-13 21:17:31 +0300

    review fixes

--- src/modules/hooks/index.tsx
+++ src/modules/hooks/index.tsx
@@ -1,12 +1,13 @@
 import React, { ReactElement } from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import { AppRoute } from 'routes/routes';
+import { AppUrls } from 'routes/appUrls';
 
 export const usePrivateRoute = (
   routerProps: AppRoute,
   isLogged: boolean,
   key: number,
 ): ReactElement => {
-  if (!isLogged && routerProps.isPrivate) return <Redirect key={key} to="/sign-in"/>;
+  if (!isLogged && routerProps.isPrivate) return <Redirect key={key} to={AppUrls.SignIn}/>;
   return <Route {...routerProps} key={key}/>;
 };
