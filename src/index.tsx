91b885312c9c14b249b7a96df9904c7863c0c69a
Вадим Васильевич <imbalance777@gmail.com>
2020-11-26 16:44:59 +0300

    refactor / move sw to modules

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { App } from 'components/App';
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize.min';
-import * as serviceWorker from 'serviceWorker';
+import * as serviceWorker from 'modules/serviceWorker';
 
 ReactDOM.render(<StrictMode><App /></StrictMode>, document.getElementById('root'));
 
