e530e1b52c5b5e69beab94f4f036403cc0cb0471
Вадим Васильевич <imbalance777@gmail.com>
2020-11-26 15:45:07 +0300

    cache added

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -1,5 +1,5 @@
 export const register = (): void => {
-  if (window && 'serviceWorker' in window.navigator) {
+  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in window.navigator) {
     window.addEventListener('load', () => {
       window.navigator.serviceWorker.register('/service-worker.js').catch((error) => {
         // eslint-disable-next-line no-console
--- webpack.config.ts
+++ webpack.config.ts
@@ -3,74 +3,89 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const { GenerateSW } = require('workbox-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.tsx',
-  output: {
-    path: path.join(__dirname, '/dist'),
-    filename: 'bundle.js',
-  },
-  devServer: {
-    hot: true,
-    open: true,
-    stats: 'errors-only',
-    port: 3000,
-    historyApiFallback: true,
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx', '.tsx', '.ts'],
-    plugins: [new TsconfigPathsPlugin()],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx|tsx|ts)$/,
-        use: ['babel-loader'],
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader', {
-          loader: 'postcss-loader',
-          options: {
-            postcssOptions: {
-              plugins: [
-                [
-                  'postcss-preset-env',
-                ],
-              ],
-            },
-          },
-        },
-        ],
-      },
-      {
-        test: /\.(png|jpe?g|gif|ico)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              outputPath: 'static',
-            },
-          },
-        ],
-      },
-    ],
-  },
-  plugins: [
+module.exports = (env: undefined, { mode }: {mode: 'production' | 'development'}) => {
+  const isDevelopment = mode === 'development';
+  const isProduction = mode === 'production';
+
+  let plugins = [
     new HtmlWebpackPlugin({
       template: './www/index.html',
       favicon: './www/favicon.ico',
     }),
-    new GenerateSW({
-      clientsClaim: true,
-      skipWaiting: true,
-      maximumFileSizeToCacheInBytes: 10 * 1024 * 1024,
-      modifyURLPrefix: {
-        auto: '/',
-      },
-      cleanupOutdatedCaches: true,
-      exclude: [/\.map$/],
-      navigateFallback: '/index.html',
-    }),
-  ],
+  ];
+
+  if (isProduction) {
+    plugins = [
+      ...plugins,
+      new GenerateSW({
+        clientsClaim: true,
+        skipWaiting: true,
+        maximumFileSizeToCacheInBytes: 10 * 1024 * 1024,
+        modifyURLPrefix: {
+          auto: '/',
+        },
+        cleanupOutdatedCaches: true,
+        exclude: [/\.map$/],
+        navigateFallback: '/index.html',
+        navigationPreload: false,
+      }),
+    ];
+  }
+
+  return {
+    entry: './src/index.tsx',
+    output: {
+      path: path.join(__dirname, '/dist'),
+      filename: 'bundle.js',
+    },
+    devtool: isDevelopment ? 'source-map' : false,
+    devServer: {
+      hot: true,
+      open: true,
+      stats: 'errors-only',
+      port: 3000,
+      historyApiFallback: true,
+    },
+    resolve: {
+      extensions: ['*', '.js', '.jsx', '.tsx', '.ts'],
+      plugins: [new TsconfigPathsPlugin()],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx|tsx|ts)$/,
+          use: ['babel-loader'],
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader', {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    'postcss-preset-env',
+                  ],
+                ],
+              },
+            },
+          },
+          ],
+        },
+        {
+          test: /\.(png|jpe?g|gif|ico)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                outputPath: 'static',
+              },
+            },
+          ],
+        },
+      ],
+    },
+    plugins,
+  };
 };
