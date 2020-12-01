027ad1e71f37796565d0821d1b43c651841df861
Inna Lytkina <innayarantseva@gmail.com>
2020-12-01 20:43:03 +0300

    fix: button click bug and small improvements

--- src/game/scenes/Start.ts
+++ src/game/scenes/Start.ts
@@ -1,14 +1,11 @@
 import { CanvasHelper } from 'helpers/CanvasHelper';
-import { Position } from 'game/objects/Position';
 import { AppMode } from 'components/GameCanvas';
 import { Colors } from 'consts/colors';
 import { Rectangle } from 'consts/shapes';
 import { SceneBase } from './SceneBase';
 
 export class StartScene extends SceneBase {
-  private secondsBeforeStart = 3;
-
-  private tickDuration = 1000;
+  private countingDown = false;
 
   private buttonDimensions = {
     width: 350,
@@ -22,18 +19,13 @@ export class StartScene extends SceneBase {
     height: this.buttonDimensions.height,
   };
 
-  private buttonTopLeft: Position = {
-    x: this.startButtonRectangle.x,
-    y: this.startButtonRectangle.y,
-  };
-
   renderScene(): void {
     CanvasHelper.clear(this.context!, this.canvasSize, Colors.LightBlue);
 
     CanvasHelper.renderButton(
       this.context!,
       this.startButtonRectangle,
-      this.buttonTopLeft,
+      { text: 'Play' },
     );
 
     CanvasHelper.renderText(
@@ -49,61 +41,16 @@ export class StartScene extends SceneBase {
     );
   }
 
-  onStartButtonClick(nextScene: (appMode: AppMode) => void): void {
-    const {
-      context, canvasSize, secondsBeforeStart, tickDuration,
-    } = this;
-    let counter = secondsBeforeStart;
-
-    // draw countdown before the game starts
-    let timerId = setTimeout(function tick() {
-      const counterText = counter === 0 ? 'GO!' : counter.toString();
-
-      CanvasHelper.clear(context!, canvasSize, Colors.LightBlue);
-
-      CanvasHelper.renderText(
-        context!,
-        'Get ready in',
-        {
-          x: canvasSize.width / 2,
-          y: canvasSize.height / 3,
-          align: 'center',
-          font: '32px Arial',
-          color: Colors.DarkGrey,
-        },
-      );
-
-      CanvasHelper.renderText(
-        context!,
-        counterText,
-        {
-          x: canvasSize.width / 2,
-          y: canvasSize.height / 2,
-          align: 'center',
-          font: '72px Arial',
-          color: Colors.DarkBlue,
-        },
-      );
-
-      counter -= 1;
-      timerId = setTimeout(tick, tickDuration);
-    }, 0);
-
-    setTimeout(() => {
-      clearInterval(timerId);
-      nextScene(AppMode.Game);
-    }, (secondsBeforeStart + 1) * tickDuration);
-  }
-
   handleCanvasClick = (nextScene: (appMode: AppMode) => void) => (event: MouseEvent) => {
     const isButtonClicked = CanvasHelper.isClickedInsideRect(
       event,
       this.clientRect,
       this.startButtonRectangle,
-    );
+    ) && !this.countingDown;
 
     if (isButtonClicked) {
-      this.onStartButtonClick(nextScene);
+      this.countingDown = true;
+      super.renderCountdown(nextScene);
     }
   };
 
