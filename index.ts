ff128161fc2f2bf5caf154163f7df35a26dcbfdb
Вадим Васильевич <imbalance777@gmail.com>
2021-01-14 16:56:32 +0300

    https added

--- index.ts
+++ index.ts
@@ -1,8 +1,21 @@
+const https = require('https');
+const fs = require('fs');
 const { app } = require('./dist/server.js');
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  // eslint-disable-next-line no-console
-  console.log('Application is started on localhost:', port);
-});
+if (process.env.NODE_ENV === 'development') {
+  https.createServer({
+    key: fs.readFileSync('server/certificate/server.key'),
+    cert: fs.readFileSync('server/certificate/server.cert'),
+  }, app)
+    .listen(port, () => {
+      // eslint-disable-next-line no-console
+      console.log('Application is started on localhost:', port);
+    });
+} else {
+  app.listen(port, () => {
+    // eslint-disable-next-line no-console
+    console.log('Application is started on localhost:', port);
+  });
+}
