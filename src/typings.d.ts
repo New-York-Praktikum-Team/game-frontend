9e6f2c204d18c8bf56e4aa53ec089b77b9faa80d
Vadim Konchik <imbalance777@gmail.com>
2021-01-18 14:04:34 +0300

    Task 74 ssr redux (#105)
    * add isServer helper
    
    * add ssr preloaded state

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,3 +1,11 @@
+import { RootState } from 'store/rootReducer';
+
 declare module '*.png';
 declare module 'react-detect-offline';
 declare module 'react-country-flag';
+
+declare global {
+  interface Window {
+    __INITIAL_STATE__: RootState;
+  }
+}
