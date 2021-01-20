9ea3a1052eac4b9a94a9d51a25cd1ff3c381f844
Вадим Васильевич <imbalance777@gmail.com>
2021-01-20 10:11:28 +0300

    remove certificates from repo

--- index.ts
+++ index.ts
@@ -4,7 +4,9 @@ const { app } = require('./dist/server.js');
 
 const port = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV === 'development') {
+const certificateIsExist = fs.existsSync('server/certificate/server.key') && fs.existsSync('server/certificate/server.cert');
+
+if (process.env.NODE_ENV === 'development' && certificateIsExist) {
   https.createServer({
     key: fs.readFileSync('server/certificate/server.key'),
     cert: fs.readFileSync('server/certificate/server.cert'),
