82b6bdd6fe3839fd8e0a12e0b4e1a4eb99263821
Вадим Васильевич <imbalance777@gmail.com>
2021-01-28 14:18:30 +0300

    add-typeorm

--- index.ts
+++ index.ts
@@ -1,23 +1,28 @@
-const https = require('https');
-const fs = require('fs');
-const { app } = require('./dist/server.js');
+import https from 'https';
+import fs from 'fs';
+// @ts-ignore
+import { app } from './dist/server.js';
+import { db } from './server/db';
 
 const port = process.env.PORT || 5000;
-
 const certificateIsExist = fs.existsSync('server/certificate/server.key') && fs.existsSync('server/certificate/server.cert');
 
-if (process.env.NODE_ENV === 'development' && certificateIsExist) {
-  https.createServer({
-    key: fs.readFileSync('server/certificate/server.key'),
-    cert: fs.readFileSync('server/certificate/server.cert'),
-  }, app)
-    .listen(port, () => {
+(async () => {
+  await db.connect();
+
+  if (process.env.NODE_ENV === 'development' && certificateIsExist) {
+    https.createServer({
+      key: fs.readFileSync('server/certificate/server.key'),
+      cert: fs.readFileSync('server/certificate/server.cert'),
+    }, app)
+      .listen(port, () => {
+        // eslint-disable-next-line no-console
+        console.log('Application is started on localhost HTTPS:', port);
+      });
+  } else {
+    app.listen(port, () => {
       // eslint-disable-next-line no-console
-      console.log('Application is started on localhost HTTPS:', port);
+      console.log('Application is started on localhost HTTP:', port);
     });
-} else {
-  app.listen(port, () => {
-    // eslint-disable-next-line no-console
-    console.log('Application is started on localhost HTTP:', port);
-  });
-}
+  }
+})();
---
+++ server/db/index.ts
@@ -0,0 +1,41 @@
+import { Connection, createConnection } from 'typeorm';
+
+class DB {
+  public mongodb!: Connection;
+
+  public postgres!: Connection;
+
+  public connect = async () => {
+    try {
+      this.postgres = await createConnection({
+        name: 'postgres',
+        type: 'postgres',
+        url: 'postgres://nyma:nyma@localhost:5436/nyma-api',
+        synchronize: true,
+      });
+      // eslint-disable-next-line no-console
+      console.log(`connection to DB success: ${this.postgres.name}`);
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.error(err);
+    }
+
+    try {
+      this.mongodb = await createConnection({
+        name: 'mongodb',
+        type: 'mongodb',
+        url: 'mongodb://localhost:27017/nyma-api',
+        useUnifiedTopology: true,
+        synchronize: true,
+      });
+
+      // eslint-disable-next-line no-console
+      console.log(`connection to DB success: ${this.mongodb.name}`);
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.error(err);
+    }
+  };
+}
+
+export const db = new DB();
